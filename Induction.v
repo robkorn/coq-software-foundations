35119935b4398c6e53344367693a8be7d216411e
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2018-10-14 14:21:55 -0400

    Implemented mult_comm.
    Lesson of the day: Coq's rewriting seems to be a bit stupid at times.

--- Induction.v
+++ Induction.v
@@ -73,3 +73,54 @@ Proof.
 (*   - simpl. reflexivity. *)
 (*   - induction n' as [| n'']. *)
 
+
+Theorem plus_rearrange : forall n m p q : nat,
+  (n + m) + (p + q) = (m + n) + (p + q).
+Proof.
+  intros n m p q.
+  assert (H : n + m = m + n).
+    { rewrite -> plus_comm. reflexivity. }
+    rewrite -> H. reflexivity. Qed.
+
+Theorem plus_swap : forall n m p : nat,
+  n + (m + p) = m + (n + p).
+Proof.
+  intros n m p.
+  rewrite plus_assoc.
+  assert (H : n + m = m + n).
+         { rewrite plus_comm. reflexivity. }
+         rewrite H. rewrite plus_assoc. reflexivity. Qed.
+
+
+Theorem mult_Sm_n : forall m n : nat,
+        S m * n = n * m + n.
+Proof.
+  intros m n.
+  induction n as [|n' IHn'].
+  - simpl. rewrite <- mult_n_O. reflexivity.
+  - rewrite <- mult_n_Sm.
+    rewrite <- plus_n_Sm.
+    rewrite IHn'. simpl.
+    rewrite <- plus_n_Sm.
+    assert (H : m + (n' * m) + n' = (n' * m) + n' + m).
+    { rewrite plus_comm.
+      rewrite plus_assoc.
+      rewrite plus_comm.
+      rewrite plus_assoc.
+      reflexivity. }
+    rewrite H.
+    reflexivity.
+Qed.
+
+Theorem mult_comm : forall m n : nat,
+  m * n = n * m.
+Proof.
+  intros m n.
+  induction m as [|m' IHm'].
+  - rewrite mult_0_l. rewrite mult_0_r. reflexivity.
+  - rewrite <- mult_n_Sm. rewrite mult_Sm_n. reflexivity. Qed.
+
+
+                   
+
+
